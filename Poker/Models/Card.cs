ab8d603b4e67bce7cf4e5d92e13e7e6df23654ad
Svetoslav Dodev <svetoslav.dodev@gmail.com>
2016-01-12 12:20:08 +0200

    Added classes Card and Deck, and enumerations CardsRank and Suits

---
+++ Poker/Models/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Poker.Models
+{
+    public class Card
+    {
+        public Card(Suits suit, CardsRank rank, Image cardImage)
+        {
+            this.Suit = suit;
+            this.Rank = rank;
+            this.CardImage = cardImage;
+        }
+
+        public Suits Suit { get; private set; }
+        public CardsRank Rank { get; private set; }
+        public Image CardImage { get; private set; }
+    }
+}
