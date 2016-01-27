dd531fb7500a7117497857d3709dab1445484681
mbeleva88 <m_d_b@mail.bg>
2016-01-27 17:51:01 +0200

    Changed integer values in CardsRank enumeration

--- Poker/Models/Enums/CardsRank.cs
+++ Poker/Models/Enums/CardsRank.cs
@@ -1,25 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Poker.Models
+﻿namespace Poker.Models
 {
     public enum CardsRank
     {
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        Ten,
-        Jack,
-        Queen,
-        King,
-        Ace
+        Two = 1,
+        Three = 2,
+        Four = 3,
+        Five = 4,
+        Six = 5,
+        Seven = 6,
+        Eight = 7,
+        Nine = 8,
+        Ten = 9,
+        Jack = 10,
+        Queen = 11,
+        King = 12,
+        Ace = 13
     }
 }
